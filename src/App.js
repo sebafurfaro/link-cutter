1e5d804a76662539264d09a127cd27198b8617f0
Sebastian Furfaro <seba.furfaro@gmail.com>
2023-05-24 16:54:00 -0300

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
 }
+
+const App = () => {
+  const [originalLink, setOriginalLink] = useState('');
+  const [cutLink, setCutLink] = useState('');
+
+  const handleCutLink = () => {
+    const randomString = generateRandomString(9);
+    const cut = `cut.now/${randomString}`;
+    setCutLink(cut);
+  };
+
+  return (
+    <section>
+      <h1>Link Cutter</h1>
+      <main>
+        <div className='form'>
+          <input
+            type="url"
+            name="linkcutter"
+            id="linkcutter"
+            placeholder="Enter link"
+            value={originalLink}
+            onChange={(e) => setOriginalLink(e.target.value)}
+          />
+          <button onClick={handleCutLink}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M6 7m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+              <path d="M6 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+              <path d="M8.6 8.6l10.4 10.4" />
+              <path d="M8.6 15.4l10.4 -10.4" />
+            </svg>
+          </button>
+        </div>
+        {cutLink && (
+          <div className='output'>
+            <p>
+              Cut Link:{' '}
+              <a href={originalLink} target="_blank" rel="noopener noreferrer">
+                {cutLink}
+              </a>
+            </p>
+          </div>
+        )}
+      </main>
+    </section>
+  );
+};
 
 export default App;
